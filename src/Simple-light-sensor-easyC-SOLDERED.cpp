056a7a09c10ed473deb71ceb9771dabc449c1eb7
Goran Jurić <74900926+gogi640@users.noreply.github.com>
2022-02-01 10:21:11 +0100

    Add files via upload

---
+++ src/Simple-light-sensor-easyC-SOLDERED.cpp
@@ -0,0 +1,95 @@
+/**
+ **************************************************
+ *
+ * @file        Simple-light-sensor-easyC-SOLDERED.cpp
+ * @brief       Example functions to overload in base class.
+ *
+ *
+ * @copyright   GNU General Public License v3.0
+ * @authors     Goran Juric @ soldered.com
+ ***************************************************/
+
+
+#include "Simple-light-sensor-easyC-SOLDERED.h"
+
+/**
+ * @brief                   Default constructor.
+ *
+ */
+SimpleLightSensor::SimpleLightSensor()
+{
+}
+
+/**
+ * @brief                   Sensor specific native constructor.
+ *
+ * @param int _pin          Pin on which is sensor connected.
+ */
+SimpleLightSensor::SimpleLightSensor(int _pin)
+{
+    pin = _pin;
+    native = 1;
+}
+
+/**
+ * @brief                   Overloaded function for virtual in base class to initialize sensor specific.
+ */
+void SimpleLightSensor::initializeNative()
+{
+    pinMode(pin, INPUT);
+}
+
+/**
+ * @brief       Function for reading value of LDR
+ *
+ * @return      value of LDR
+ */
+uint16_t SimpleLightSensor::getValue()
+{
+    if (!native)
+    {
+        readRegister(ANALOG_READ_REG, raw, 2 * sizeof(uint8_t));
+        return raw[0] | (raw[1]) << 8;
+    }
+    return analogRead(pin);
+}
+
+/**
+ * @brief       Function for calculating resistance of LDR
+ *
+ * @return      resistance of LDR
+ */
+float SimpleLightSensor::getResistance()
+{
+    uint16_t temp = getValue();
+    if (temp != 0)
+    {
+        return R * (ADC_width - temp) / (float)temp;
+    }
+    return 0;
+}
+
+/**
+ * @brief       Function for calculating value of light intensity in Lux
+ *
+ * @return      light intensity in lux
+ */
+float SimpleLightSensor::getLux()
+{
+    uint16_t temp = getResistance();
+    if (temp != 0)
+    {
+        return 12500000.0 * pow(temp, -1.4);
+    }
+    return 0;
+}
+
+/**
+ * @brief       Function for setting ADC bit width of microcontroller
+ *
+ * @param       uint8_t _ADC_width ADC bit width in bits
+ */
+void SimpleLightSensor::setADCWidth(uint8_t _ADC_width)
+{
+    ADC_width = pow(2, _ADC_width) - 1;
+}
