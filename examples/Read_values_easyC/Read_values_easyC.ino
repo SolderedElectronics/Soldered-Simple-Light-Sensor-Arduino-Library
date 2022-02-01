2501086fb18ca73d7e116f896283c9d0f52b9235
Goran Jurić <74900926+gogi640@users.noreply.github.com>
2022-02-01 10:22:00 +0100

    Add files via upload

--- examples/Read_values_easyC/Read_values_easyC.ino
+++ examples/Read_values_easyC/Read_values_easyC.ino
@@ -1,50 +1,42 @@
 /**
  **************************************************
  *
- *  @file        Read_values_easyC.ino
- *  @brief       Example for using the digital and analog read functions for Simple fire sensor with easyC
+ * @file        Read_values_easyC.ino
+ * @brief       Example for using the digital and analog read functions for Simple light sensor with easyC
  *
  *
- * 	product: www.solde.red/333077
+ *	product: www.solde.red/333076
  *
- *  @authors     Goran Juric for Soldered.com
+ * @authors     Goran Juric for Soldered.com
  ***************************************************/
 
 #include "Simple-light-sensor-easyC-SOLDERED.h"
-
-#define TRESHHOLD 255
 
 // Declare the sensor object
 SimpleLightSensor sensor;
 
 void setup()
 {
-  // Initialize the serial communication via UART
-  Serial.begin(115200);
+    // Initialize the serial communication via UART
+    Serial.begin(115200);
 
-  // Initialize the sensor
-  sensor.begin();
+    // Initialize the sensor
+    sensor.begin();
 
-  // If different microcontroller with different bit width
-  // is used, it should be set using this function
-  sensor.setADCWidth(10);
+    // If different microcontroller with different bit width
+    // is used, it should be set using this function
+    sensor.setADCWidth(10);
 }
 
 void loop()
 {
+    Serial.print("Resistance of a LDR: "); // Print information message
+    Serial.print(sensor.getResistance());  // Prints percent value of slider potentiometer
+    Serial.println(" Ohms.");              // Print information message
 
-  Serial.print("IR light sensor reading: "); // Print information message
-  Serial.println(sensor.getValue());  // Prints percent value of slider potentiometer
+    Serial.print("Light intensity: "); // Print information message
+    Serial.print(sensor.getLux());     // Prints raw value of slider potentiometer
+    Serial.println(" lux.");           // Print information message
 
-  if (sensor.getValue() < TRESHOLD)
-  {
-    Serial.println("Fire is detected!!");
-  }
-
-  else
-  {
-    Serial.println("Fire is not detected.");
-  }
-  delay(1000);
-
+    delay(1000);
 }
