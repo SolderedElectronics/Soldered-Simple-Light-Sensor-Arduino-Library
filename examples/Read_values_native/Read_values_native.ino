9b3797f4cbe309b6ffbddfed58253e4cdabdf2ec
Main Push Robot <robot@example.com>
2022-02-17 06:59:14 +0000

    Committing dev changes

--- examples/Read_values_native/Read_values_native.ino
+++ examples/Read_values_native/Read_values_native.ino
@@ -1,15 +1,15 @@
 /**
  **************************************************
- *
- * @file        Read_values_native.ino
- * @brief       Example for using the digital and analog read functions for Simple Light sensor
- *
- *
- *	product: www.solde.red/333078
- *
- * @authors     Goran Juric for Soldered.com
+
+   @file        Read_values_native.ino
+   @brief       Example for using the digital and analog read functions for Simple Light sensor
+
+
+ 	product: www.solde.red/333078
+
+   @authors     Goran Juric for Soldered.com
  ***************************************************/
- 
+
 //Connecting diagram
 //
 //Breakout      Arduino
@@ -30,29 +30,29 @@ SimpleLightSensor sensor(ANALOG_PIN);
 
 void setup()
 {
-    // Initialize the serial communication via UART
-    Serial.begin(115200);
+  // Initialize the serial communication via UART
+  Serial.begin(115200);
 
-    // Initialize the sensor
-    sensor.begin();
+  // Initialize the sensor
+  sensor.begin();
 
-    // If different microcontroller with different bit width 
-    // is used, it should be set using this function
-    sensor.setADCWidth(10);
+  // If different microcontroller with different bit width
+  // is used, it should be set using this function
+  sensor.setADCWidth(10);
 }
 
 void loop()
 {
-    Serial.print("Resistance of a LDR: "); //Print information message
-    Serial.print(sensor.getResistance()); //Prints percent value of light sensor
-    Serial.println(" Ohms."); //Print information message
-    
-    Serial.print("Light intensity: "); //Print information message
-    Serial.print(sensor.getLux()); //Prints raw value of light sensor
-    Serial.println(" lux."); //Print information message
+  Serial.print("Resistance of a LDR: "); //Print information message
+  Serial.print(sensor.getResistance()); //Prints percent value of light sensor
+  Serial.println(" Ohms."); //Print information message
 
-    //You can adjust treshold light intesity using potentiometer on breakout board
-    Serial.println(digitalRead(DIGITAL_PIN) ? "Treshold light intesity is past." : "Treshold intensity is not past.");
+  Serial.print("Light intensity: "); //Print information message
+  Serial.print(sensor.getLux()); //Prints raw value of light sensor
+  Serial.println(" lux."); //Print information message
 
-    delay(1000);
+  //You can adjust treshold light intesity using potentiometer on breakout board
+  Serial.println(digitalRead(DIGITAL_PIN) ? "Treshold light intesity is past." : "Treshold intensity is not past.");
+
+  delay(1000);
 }
