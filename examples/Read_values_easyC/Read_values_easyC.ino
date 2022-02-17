0f0a30ec07d99afeccbeb92e264de16fc035d097
Goran Jurić <74900926+gogi640@users.noreply.github.com>
2022-02-17 07:58:56 +0100

    Examples formatted

--- examples/Read_values_easyC/Read_values_easyC.ino
+++ examples/Read_values_easyC/Read_values_easyC.ino
@@ -1,13 +1,13 @@
 /**
  **************************************************
- *
- * @file        Read_values_easyC.ino
- * @brief       Example for using the digital and analog read functions for Simple light sensor with easyC
- *
- *
- *	product: www.solde.red/333076
- *
- * @authors     Goran Juric for Soldered.com
+
+   @file        Read_values_easyC.ino
+   @brief       Example for using the digital and analog read functions for Simple light sensor with easyC
+
+
+ 	product: www.solde.red/333076
+
+   @authors     Goran Juric for Soldered.com
  ***************************************************/
 
 #include "Simple-light-sensor-easyC-SOLDERED.h"
@@ -17,24 +17,24 @@ SimpleLightSensor sensor;
 
 void setup()
 {
-    // Initialize the serial communication via UART with 115200 baud rate
-    //and it is needed to set same baud rate in serial monitor if it is used
-    Serial.begin(115200);
+  // Initialize the serial communication via UART with 115200 baud rate
+  //and it is needed to set same baud rate in serial monitor if it is used
+  Serial.begin(115200);
 
-    // Initialize the sensor for use
-    sensor.begin();
+  // Initialize the sensor for use
+  sensor.begin();
 
 }
 
 void loop()
 {
-    Serial.print("Resistance of a LDR: "); // Print information message
-    Serial.print(sensor.getResistance());  // Prints percent value of light sensor
-    Serial.println(" Ohms.");              // Print information message
+  Serial.print("Resistance of a LDR: "); // Print information message
+  Serial.print(sensor.getResistance());  // Prints percent value of light sensor
+  Serial.println(" Ohms.");              // Print information message
 
-    Serial.print("Light intensity: "); // Print information message
-    Serial.print(sensor.getLux());     // Prints raw value of light sensor
-    Serial.println(" lux.");           // Print information message
+  Serial.print("Light intensity: "); // Print information message
+  Serial.print(sensor.getLux());     // Prints raw value of light sensor
+  Serial.println(" lux.");           // Print information message
 
-    delay(1000);
+  delay(1000);
 }
