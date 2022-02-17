4cb36341df21408ec206c3fac254a93c25d63367
Docs Robot <robot@example.com>
2022-02-17 12:00:59 +0000

    Committing documentation changes

--- docs/dir_3db0dda00e9f66085ea9403aa7e09fb8.js
+++ docs/dir_3db0dda00e9f66085ea9403aa7e09fb8.js
@@ -1,6 +1,5 @@
 var dir_3db0dda00e9f66085ea9403aa7e09fb8 =
 [
-    [ "easyC.cpp", "easy_c_8cpp.html", null ],
     [ "easyC.h", "easy_c_8h.html", [
       [ "SIMPLE_LIGHT_SENSOR_EASYC::EasyC", "class_s_i_m_p_l_e___l_i_g_h_t___s_e_n_s_o_r___e_a_s_y_c_1_1_easy_c.html", "class_s_i_m_p_l_e___l_i_g_h_t___s_e_n_s_o_r___e_a_s_y_c_1_1_easy_c" ]
     ] ]
--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['easyc_0',['EasyC',['../class_s_i_m_p_l_e___l_i_g_h_t___s_e_n_s_o_r___e_a_s_y_c_1_1_easy_c.html#a6c95231404f336a85efd1933dce77326',1,'SIMPLE_LIGHT_SENSOR_EASYC::EasyC::EasyC()'],['../class_s_i_m_p_l_e___l_i_g_h_t___s_e_n_s_o_r___e_a_s_y_c_1_1_easy_c.html',1,'SIMPLE_LIGHT_SENSOR_EASYC::EasyC']]],
-  ['easyc_2ecpp_1',['easyC.cpp',['../easy_c_8cpp.html',1,'']]],
-  ['easyc_2eh_2',['easyC.h',['../easy_c_8h.html',1,'']]],
-  ['err_3',['err',['../class_s_i_m_p_l_e___l_i_g_h_t___s_e_n_s_o_r___e_a_s_y_c_1_1_easy_c.html#a7a151c83614e339126d321e18531d3f2',1,'SIMPLE_LIGHT_SENSOR_EASYC::EasyC']]]
+  ['easyc_2eh_1',['easyC.h',['../easy_c_8h.html',1,'']]],
+  ['err_2',['err',['../class_s_i_m_p_l_e___l_i_g_h_t___s_e_n_s_o_r___e_a_s_y_c_1_1_easy_c.html#a7a151c83614e339126d321e18531d3f2',1,'SIMPLE_LIGHT_SENSOR_EASYC::EasyC']]]
 ];
