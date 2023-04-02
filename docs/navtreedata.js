757ce56fecab1189aa601c82393211888f3bb508
Docs Robot <robot@example.com>
2023-04-02 10:41:04 +0000

    Committing documentation changes

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -25,7 +25,7 @@
 var NAVTREE =
 [
   [ "Simple light sensor easyC Library", "index.html", [
-    [ "Simple light sensor board with easyC Arduino library", "index.html", "index" ],
+    [ "Simple light sensor board Arduino library", "index.html", "index" ],
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
--- docs/search/all_9.js
+++ docs/search/all_9.js
@@ -4,7 +4,7 @@ var searchData=
   ['senddata_1',['sendData',['../class_easy_c.html#afe8aa014ae53cad3b0f3c42f59277195',1,'EasyC']]],
   ['setadcwidth_2',['setADCWidth',['../class_simple_light_sensor.html#a3d005029b169054da69428e9d2e3a4b6',1,'SimpleLightSensor']]],
   ['setthreshold_3',['setThreshold',['../class_simple_light_sensor.html#a5565973e5706a383f5c0816905fa2a18',1,'SimpleLightSensor']]],
-  ['simple_20light_20sensor_20board_20with_20easyc_20arduino_20library_4',['Simple light sensor board with easyC Arduino library',['../index.html',1,'']]],
+  ['simple_20light_20sensor_20board_20arduino_20library_4',['Simple light sensor board Arduino library',['../index.html',1,'']]],
   ['simple_2dlight_2dsensor_2deasyc_2dsoldered_2ecpp_5',['Simple-light-sensor-easyC-SOLDERED.cpp',['../_simple-light-sensor-easy_c-_s_o_l_d_e_r_e_d_8cpp.html',1,'']]],
   ['simple_2dlight_2dsensor_2deasyc_2dsoldered_2eh_6',['Simple-light-sensor-easyC-SOLDERED.h',['../_simple-light-sensor-easy_c-_s_o_l_d_e_r_e_d_8h.html',1,'']]],
   ['simplelightsensor_7',['SimpleLightSensor',['../class_simple_light_sensor.html',1,'SimpleLightSensor'],['../class_simple_light_sensor.html#a85370c3ec14cac5c9dac0937ffe9d465',1,'SimpleLightSensor::SimpleLightSensor()'],['../class_simple_light_sensor.html#a64d9818274c10285055ec5622f981f98',1,'SimpleLightSensor::SimpleLightSensor(int)']]]
