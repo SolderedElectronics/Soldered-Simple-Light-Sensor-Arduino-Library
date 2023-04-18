07c4514f76619a5ccd2b157cc100f37c7df52e35
Docs Robot <robot@example.com>
2023-04-18 06:46:10 +0000

    Committing documentation changes

--- docs/search/functions_2.js
+++ docs/search/functions_2.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['getlux_0',['getLux',['../class_simple_light_sensor.html#a2e2d938e80975ae158b8f29008b530e4',1,'SimpleLightSensor']]],
-  ['getresistance_1',['getResistance',['../class_simple_light_sensor.html#a8a3e5274bef579ef91e6256a11eb743a',1,'SimpleLightSensor']]],
-  ['getvalue_2',['getValue',['../class_simple_light_sensor.html#a1c0c4ed7fec63161dd17ce77ae788e51',1,'SimpleLightSensor']]]
+  ['getrawvalue_1',['getRawValue',['../class_simple_light_sensor.html#acd885b1219c170ae984fd77f88169cb9',1,'SimpleLightSensor']]],
+  ['getresistance_2',['getResistance',['../class_simple_light_sensor.html#a8a3e5274bef579ef91e6256a11eb743a',1,'SimpleLightSensor']]],
+  ['getvalue_3',['getValue',['../class_simple_light_sensor.html#abda0248e0ee1f6a6178850be329587f7',1,'SimpleLightSensor']]]
 ];
--- docs/search/functions_5.js
+++ docs/search/functions_5.js
@@ -3,6 +3,7 @@ var searchData=
   ['sendaddress_0',['sendAddress',['../class_easy_c.html#a10aa7df19e8c7bf12cd720b662f317a3',1,'EasyC']]],
   ['senddata_1',['sendData',['../class_easy_c.html#afe8aa014ae53cad3b0f3c42f59277195',1,'EasyC']]],
   ['setadcwidth_2',['setADCWidth',['../class_simple_light_sensor.html#a3d005029b169054da69428e9d2e3a4b6',1,'SimpleLightSensor']]],
-  ['setthreshold_3',['setThreshold',['../class_simple_light_sensor.html#a5565973e5706a383f5c0816905fa2a18',1,'SimpleLightSensor']]],
-  ['simplelightsensor_4',['SimpleLightSensor',['../class_simple_light_sensor.html#a85370c3ec14cac5c9dac0937ffe9d465',1,'SimpleLightSensor::SimpleLightSensor()'],['../class_simple_light_sensor.html#a64d9818274c10285055ec5622f981f98',1,'SimpleLightSensor::SimpleLightSensor(int)']]]
+  ['setrawthreshold_3',['setRawThreshold',['../class_simple_light_sensor.html#a12d9dd4358ce9f18c9e8b553c92fd293',1,'SimpleLightSensor']]],
+  ['setthreshold_4',['setThreshold',['../class_simple_light_sensor.html#a5826ada87e4bbbb6eae5a56d068b595a',1,'SimpleLightSensor']]],
+  ['simplelightsensor_5',['SimpleLightSensor',['../class_simple_light_sensor.html#a85370c3ec14cac5c9dac0937ffe9d465',1,'SimpleLightSensor::SimpleLightSensor()'],['../class_simple_light_sensor.html#a64d9818274c10285055ec5622f981f98',1,'SimpleLightSensor::SimpleLightSensor(int)']]]
 ];
